e3e01d34c3fdf2218af55ba662fece0f1631b9e4
Spencer Holladay <spencer.holladay@gmail.com>
2019-03-23 19:55:56 -0400

    adds video header and header style

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+// function deferVideo() {
+
+//     //defer html5 video loading
+//   $("video source").each(function() {
+//     var sourceFile = $(this).attr("data-src");
+//     $(this).attr("src", sourceFile);
+//     var video = this.parentElement;
+//     video.load();
+//     video.play();
+//   });
+
+// }
+// window.onload = deferVideo;
