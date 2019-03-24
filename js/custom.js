22b626dae83332cb7f54b1f6272e2cc55ca0c864
Spencer Holladay <spencer.holladay@gmail.com>
2019-03-24 13:10:00 -0400

    adds card stlying

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,13 @@
+
+// window.addEventListener('scroll', function (e) {
+//     var scrolled = window.pageYOffset;
+//     const background = document.querySelector(".p-img");
+//     background.style.top = - (scrolled * 0.2) + 'px';
+// });
+
 window.addEventListener('scroll', function (e) {
     var scrolled = window.pageYOffset;
     const background = document.querySelector(".p-img");
-    background.style.top = (scrolled * 0.2) + 'px');
+    var coords = '0% '+ (- (scrolled *0.2) + 'px');
+    background.style.top  = coords;
 });
